0939e89022cde4a2f73f6e1a876ade90847bc669
Tiny Penguin <chase.seeberger@gmail.com>
2023-12-09 11:34:34 -0800

    starting api script

--- Assets/js/weather-api.js
+++ Assets/js/weather-api.js
@@ -1,1 +1,15 @@
 const apikey = "fa4a15873725d3e5be6c755a8f395ec9";
+
+const apiURL = "https://api.openweathermap.org/data/2.5/forecast/"
+
+const city = "";
+
+const cityInput = document.getElementById("city");
+
+const searchBtn = document.getElementById("search");
+
+searchBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log(cityInput.value);
+    console.log("clicked");
+});
