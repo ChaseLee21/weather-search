958bb590bbdd46ca3a22a93e47168b3386aff513
Tiny Penguin <chase.seeberger@gmail.com>
2023-12-11 10:13:47 -0800

    refactoring script to practice clean coding

--- Assets/js/weather-api.js
+++ Assets/js/weather-api.js
@@ -1,114 +1,61 @@
-//api variables
 const apikey = "fa4a15873725d3e5be6c755a8f395ec9";
 const apiURL = "https://api.openweathermap.org/data/2.5/forecast"
-
-//html variables
 const cityInput = document.getElementById("city");
 const searchBtn = document.getElementById("search");
 const forecastDisplay = document.getElementById("forecastDisplay");
-
-//query variables
+const recentSearchesContainer = document.getElementById("recentSearches");
 let city = "";
-let query = apiURL + "?q=" + city + "&appid=" + apikey;
-
-//api data variables
+let query = "";
 let days = [[], [], [], [], []];
 
 searchBtn.addEventListener("click", async function (event) {
     event.preventDefault();
-    query = setQuery();
-    await getWeather();
-    displayForecast();
+    days = await getWeather(setQuery());
+    displayWeather(days);
     document.getElementById("cityName").innerHTML = cityInput.value.trim();
     saveSearch(cityInput.value.trim());
-    displayRecentSearches();
 });
 
+// input: user input from the city input field
+// returns: api query
 function setQuery() {
     city = cityInput.value.trim();
     return apiURL + "?q=" + city + "&appid=" + apikey + "&units=imperial";
 }
 
-async function getWeather() {
-    console.log(query);
+// api call to openweathermap.org
+// input: api query
+// returns: days[day][segment]
+// 5 days, 8 segments per day
+async function getWeather(query) {
     await fetch(query)
         .then(function (response) {
             return response.json();
         })
-        .then(function (data) {
-            // keeps track of what day we are on
+        .then(function (segments) {
             let dayCount = 0;
-
-            // loop through all the data returned from the api
-            for (let i = 0; i < data.list.length; i++) {
-                // stores the current segment of the day we are on (3 hour segment)
-                let segmentOfDay = data.list[i];
-                // stores an object which holds the date and the time of the date we are on
-                let date = createDateObject(segmentOfDay.dt_txt);
-                // pushes the segment of the day into the array based on what day it is
-                days[dayCount].push(segmentOfDay);
-                // checks if its the last segment of the day
-                if (date.time == "21:00:00") {
-                    // if it is the last segment of the day, increment the day count
-                    dayCount++;
-                }
-            }
+            days = [[], [], [], [], []];
+            segments.list.forEach((segment) => {
+                days[dayCount].push(segment);
+                if (createDateObject(segment.dt_txt).time == "21:00:00") dayCount++;
+            });
+            return days;
         });
     }
 
-// seperates a date and time string into a variable that holds the day and the time
+// date/time string => {day: day, time: time}
 function createDateObject(dateString) {
-    // splits the date and time string into an array
     let date = dateString.split(" ");
-    // stores the day and time into variables
-    let day = date[0];
-    let time = date[1];
-    // returns an object with the day and time
     return {
-        day : day,
-        time : time
+        day : date[0],
+        time : date[1]
     };
 }
 
-function displayRecentSearches() {
-    let recentSearches = JSON.parse(localStorage.getItem("recentSearches"));
-    let recentSearchesContainer = document.getElementById("recentSearches");
-    recentSearchesContainer.innerHTML = "";
-    if (recentSearches) {
-        recentSearches.forEach(search => {
-            let searchEl = document.createElement("button");
-            searchEl.setAttribute("class", "btn btn-secondary m-1");
-            searchEl.textContent = search;
-            searchEl.addEventListener("click", async function (event) {
-                event.preventDefault();
-                cityInput.value = search;
-                query = setQuery();
-                await getWeather();
-                displayForecast();
-                document.getElementById("cityName").innerHTML = cityInput.value.trim();
-            });
-            recentSearchesContainer.appendChild(searchEl);
-        });
-    }
-}
-
-function saveSearch(search) {
-    let recentSearches = JSON.parse(localStorage.getItem("recentSearches"));
-    if (!recentSearches) {
-        recentSearches = [];
-    } 
-    if (!recentSearches.includes(search)) {
-        recentSearches.push(search);
-    }
-    localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
-}
-
-function displayForecast() {
-    console.log(days);
-    // clear the forecast display
+// input: days[day][segment]
+function displayWeather(days) {
     forecastDisplay.innerHTML = "";
     days.forEach(day => {
-        // get the information we need from the day
         let maxTemp = Math.floor(getMax(day)); 
         let minTemp = Math.floor(getMin(day));
         let date = createDateObject(day[0].dt_txt).day;
@@ -116,9 +63,7 @@ function displayForecast() {
         let iconURL = "http://openweathermap.org/img/w/" + icon + ".png";
         let wind = getWind(day);
         let humidity = getHumidity(day);
-        console.log(minTemp, maxTemp, date, iconURL, wind, humidity);
 
-        //creating the elements to display the data
         let dayContainer = document.createElement("div");
         dayContainer.setAttribute("class", "card m-2 p-2");
         let dayHeader = document.createElement("h3");
@@ -132,32 +77,78 @@ function displayForecast() {
         let dayHumidity = document.createElement("p");
         dayHumidity.textContent = "Humidity: " + humidity;
 
-        //appending the elements to the page
         dayContainer.appendChild(dayHeader);
         dayContainer.appendChild(dayIcon);
         dayContainer.appendChild(dayTemp);
         dayContainer.appendChild(dayWind);
         dayContainer.appendChild(dayHumidity);
         forecastDisplay.appendChild(dayContainer);
-
     });
 
 }
 
+// input: days[day][segment]
+// returns: max wind speed
 function getWind(day){
     return day.reduce((max, p) => p.wind.speed > max ? p.wind.speed : max, day[0].wind.speed);
 }
 
+// input: days[day][segment]
+// returns: max humidity
 function getHumidity(day){
     return day.reduce((max, p) => p.main.humidity > max ? p.main.humidity : max, day[0].main.humidity);
 }
 
+// input: days[day][segment]
+// returns: max temp
 function getMax(day){
     return day.reduce((max, p) => p.main.temp > max ? p.main.temp : max, day[0].main.temp);
 }
 
+// input: days[day][segment]
+// returns: min temp
 function getMin(day){
     return day.reduce((min, p) => p.main.temp < min ? p.main.temp : min, day[0].main.temp);
 }
 
-displayRecentSearches();
+// getSearches(), displaySearches(search), saveSearch(search) work together to manage recent searches
+function getSearches() {
+    let recentSearches = JSON.parse(localStorage.getItem("recentSearches"));
+    recentSearchesContainer.innerHTML = "";
+    if (recentSearches) {
+        recentSearches.forEach(search => {
+            displaySearches(search);
+        });
+    }
+}
+
+//input search string
+function displaySearches(search) {
+    let searchEl = document.createElement("button");
+    searchEl.setAttribute("class", "btn btn-secondary m-1");
+    searchEl.textContent = search;
+    searchEl.addEventListener("click", async function (event) {
+        event.preventDefault();
+        cityInput.value = search;
+        days = await getWeather(setQuery());
+        displayWeather();
+        document.getElementById("cityName").innerHTML = cityInput.value.trim();
+        saveSearch(cityInput.value.trim());
+    });
+    recentSearchesContainer.appendChild(searchEl);
+}
+
+//input: search string
+function saveSearch(search) {
+    let recentSearches = JSON.parse(localStorage.getItem("recentSearches"));
+    if (!recentSearches) {
+        recentSearches = [];
+    } 
+    if (!recentSearches.includes(search)) {
+        recentSearches.push(search);
+    }
+    localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
+    getSearches();
+}
+
+getSearches();
